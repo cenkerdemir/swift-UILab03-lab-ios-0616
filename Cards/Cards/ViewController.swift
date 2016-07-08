4f45e2ee74a947f458803fe11b1115299d411f11
github username <github email address>
2016-07-08 09:16:06 -0400

    lab complete

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -11,9 +11,37 @@ import UIKit
 class ViewController: UIViewController {
     // TODO: Create IB outlets
 
+    @IBOutlet weak var topCornerSuit: UILabel!
+    @IBOutlet weak var bottomCornerSuit: UILabel!
+    @IBOutlet weak var middleRank: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     // TODO: IB actions and code to update UI
+    
+    @IBAction func clubsOfFour(sender: AnyObject) {
+        topCornerSuit.text = "♣️"
+        bottomCornerSuit.text = "♣️"
+        middleRank.text = "4"
+    }
+    
+    @IBAction func spadesOfThree(sender: AnyObject) {
+        topCornerSuit.text = "♠️"
+        bottomCornerSuit.text = "♠️"
+        middleRank.text = "3"
+    }
+
+    @IBAction func diamondsOfEight(sender: AnyObject) {
+        topCornerSuit.text = "♦️"
+        bottomCornerSuit.text = "♦️"
+        middleRank.text = "8"
+    }
+    
+    @IBAction func heartsOfTen(sender: AnyObject) {
+        topCornerSuit.text = "♥️"
+        bottomCornerSuit.text = "♥️"
+        middleRank.text = "10"
+    }
 }
